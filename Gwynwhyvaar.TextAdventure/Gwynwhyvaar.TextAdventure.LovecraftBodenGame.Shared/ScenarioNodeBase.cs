c09fcba7bab165e5d854f34231cfc318f589cd3d
Gwynwhyvaar <tunde.ayoko@hotmail.com>
2025-04-23 12:36:31 +0100

    - updated ignore file to exclude intellij settings.

--- Gwynwhyvaar.TextAdventure/Gwynwhyvaar.TextAdventure.LovecraftBodenGame.Shared/ScenarioNodeBase.cs
+++ Gwynwhyvaar.TextAdventure/Gwynwhyvaar.TextAdventure.LovecraftBodenGame.Shared/ScenarioNodeBase.cs
@@ -5,7 +5,7 @@ namespace Gwynwhyvaar.TextAdventure.LovecraftBodenGame.Shared
     public class ScenarioNodeBase
     {
         public string AddressGuid { get; set; } = Guid.NewGuid().ToString("X");
-        public DateTime CreatedDate { get; set; } = DateTime.Now;   
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
         public string? Name { get; set; }
     }
 }
